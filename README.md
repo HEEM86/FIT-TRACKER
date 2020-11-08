796e41cd88a1fc2fffb20b433b58e2565abc8d2f
Slice Wess <ernestwesson86@gmail.com>
2020-11-07 23:27:56 -0800

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# FIT-TRACKER
+## FIT-TRACKER :fire:
+
+![fitness pic](https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80)
+
+
+## Description :bulb:
+
+The FIT-Tracker app allows the user to create customize workout system to achieve optimal results
+
+**The user can add the following information to create their workout and track their progress**
+
+- Name
+- Date
+- Weight
+- Execercise
+- Sets
+- Reps
+- Duration
+- Distance
+
+
+This app also displays charts and graphs so the user can analyze their progression
+
+## Challenges! :warning:
+
+- Wasn't able to activate the dashboard section after entering the user workout info
+
+-
+
+
+
+## Credits :star:
+
+
+[W3 Schools Documentation](w3schools.com)
+
+
+[Lofi Music](https://www.youtube.com/watch?v=tutZKLeGrCs)
+
+## Author :pen:
+
+Ernest Wesson :zap:
+
+LinkedIN: https://www.linkedin.com/in/ernest-wesson-b4183b5a/
+
+
+
+
+## License :clipboard:
+
+
+MIT License
+
+Copyright (c) [2020] [Ernest]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,64 +1,60 @@
 const router = require("express").Router();
 const Fitness = require("../models/fit-plan.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-  Fitness.create(body)
-    .then(dbWorkout => {
+router.post("/api/workouts", (req, res) => {
+  Fitness.create({})
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
 
-  router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Fitness.findByIdAndUpdate(params.id,
-  {$push:{exercises: body}},
-  {new: true, runValidators: true}  
-  ).then(dbWorkout => {
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Fitness.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
-  });
+});
 
-
-    router.get("/api/workouts/range", (req, res) => {
-    Fitness.find().limit(10)
-    .then(dbWorkout => {
+router.get("/api/workouts/range", (req, res) => {
+  console.log(res)
+  Fitness.find()
+    .limit(7)
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json(err);
     });
-  });
-    router.get("/api/workouts", (req, res) => {
-      Fitness.find()
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-    });
-  
+});
 
-    router.delete("/api/workouts/", ({body}, res) => {
-      Fitness.findByIdAndDelete(body.id)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
+router.get("/api/workouts", (req, res) => {
+  Fitness.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
-       
-          
-    
+});
 
+router.delete("/api/workouts/", ({ body }, res) => {
+  Fitness.findByIdAndDelete(body.id)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 module.exports = router;
-
-
-    
